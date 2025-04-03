623200c934bdf8af4faf38fa7dde0a4bcbd4dcaf
Vishal <donvip56@gmail.com>
2025-04-03 21:24:50 +0530

    Add email field to User model, enhance registration validation, and implement attending events endpoint

--- models/User.js
+++ models/User.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true }, // Plain text (⚠️ Not Secure)
-    role: { type: String, default: 'user' },
+    role: { type: String, default: 'admin' },
     vtcName: String,
 });
 
